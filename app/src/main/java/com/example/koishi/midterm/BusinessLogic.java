41e9968bd032c77db13744440e3da410229fa955
Koishi <1440089139@qq.com>
2017-11-03 15:51:23 +0800

    add 业务逻辑

---
+++ app/src/main/java/com/example/koishi/midterm/BusinessLogic.java
@@ -0,0 +1,30 @@
+package com.example.koishi.midterm;
+
+/**
+ * Created by Koishi on 11/3/2017.
+ */
+
+public class BusinessLogic {
+    private String error;
+
+    public BusinessLogic() {
+
+    }
+
+    // 增删改查的四个方法 成功返回true， 不成功返回false，并设置失败原因
+    public boolean addCharacter() {
+        return false;
+    }
+
+    public boolean deleteCharacter() {
+        return false;
+    }
+
+    public boolean editCharacter() {
+        return false;
+    }
+
+    public boolean queryCharacter() {
+        return false;
+    }
+}
--- app/src/main/java/com/example/koishi/midterm/MainActivity.java
+++ app/src/main/java/com/example/koishi/midterm/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // 设置事件监听器
+    private void setEventHandlers() {
+
+    }
 }
