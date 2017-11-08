64980d1440ee48aeb6f4091e5b78f260e5debd22
Koishi <1440089139@qq.com>
2017-11-08 17:29:00 +0800

    Edit Manifest

--- app/src/main/java/com/example/koishi/midterm/DialogActivity.java
+++ app/src/main/java/com/example/koishi/midterm/DialogActivity.java
@@ -1,5 +1,7 @@
 package com.example.koishi.midterm;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,5 +9,10 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class DialogActivity extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_layout);
+        setFinishOnTouchOutside(false);
+    }
 }
