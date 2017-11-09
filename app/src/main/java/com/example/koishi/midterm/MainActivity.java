aa638296a52270ec4d6baca9fd7d7e0113a3ec78
Koishi <1440089139@qq.com>
2017-11-10 00:25:02 +0800

    修改部分UI代码 again

--- app/src/main/java/com/example/koishi/midterm/MainActivity.java
+++ app/src/main/java/com/example/koishi/midterm/MainActivity.java
@@ -5,6 +5,8 @@ import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         businessLogic = new BusinessLogic(this);
         initializeList();
+        setEventHandlers();
     }
 
     @Override
@@ -26,9 +29,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initializeList() {
+        ListView listView = findViewById(R.id.characterList);
         Cursor cursor = businessLogic.getAllCharacters();
-        String[] from = new String[]{};
-        int[] to = new int[]{};
+
+        String[] from = new String[]{
+                CharacterTable.CharacterEntry.avatarColumn,
+                CharacterTable.CharacterEntry.nameColumn,
+                CharacterTable.CharacterEntry.sexColumn,
+                CharacterTable.CharacterEntry.kingdomColumn,
+                CharacterTable.CharacterEntry.birthAndDeathYearsColumn,
+                CharacterTable.CharacterEntry.birthplaceColumn,
+        };
+
+        int[] to = new int[]{
+                R.id.avatarItem,
+                R.id.nameItem,
+                R.id.sexItem,
+                R.id.kingdomItem,
+                R.id.birthAndDeathYearsItem,
+                R.id.birthplaceItem
+        };
 
         SimpleCursorAdapter.ViewBinder viewBinder = new SimpleCursorAdapter.ViewBinder() {
             @Override
@@ -42,7 +62,14 @@ public class MainActivity extends AppCompatActivity {
 
     // 设置事件监听器
     private void setEventHandlers() {
-
+        Button button = findViewById(R.id.dialog);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
